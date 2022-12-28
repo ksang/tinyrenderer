8249b14e31ef705cd1877c491ff26750004e1666
ksang <kaitoy@qq.com>
2022-12-28 17:05:12 +0800

    add wireframe rendering

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
+#include <vector>
+#include <cmath>
 #include "tgaimage.h"
+#include "wireframe.h"
 
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red   = TGAColor(255, 0,   0,   255);
+const int width  = 800;
+const int height = 800;
 
 int main(int argc, char** argv) {
-	TGAImage image(100, 100, TGAImage::RGB);
-	image.set(52, 41, red);
+	TGAImage image(width, height, TGAImage::RGB);
+	
+	if (argc >= 2) {
+		if (std::string(argv[1]) == "wireframe") {
+			if (argc >= 3) {
+				wireframe(argv[2], &image, width, height);
+			} else {
+				wireframe("obj/african_head.obj", &image, width, height);
+			}
+		}
+	}
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
 	return 0;
